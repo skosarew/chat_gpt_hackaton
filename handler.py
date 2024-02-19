20e177803f54557115f980dd453ac5ed4fc79ab0
Evgeny Skosarev <evgeny.skosarev@adjust.com>
2024-02-19 15:37:32 +0100

    remove query from args

--- handler.py
+++ handler.py
@@ -24,7 +24,7 @@ class OpenAPIHandler:
         token_budget: int = 4096 - 500,
         model: str = 'gpt-4',
     ) -> str:
-        strings, _ = self.strings_ranked_by_relatedness(self.question)
+        strings, _ = self.strings_ranked_by_relatedness()
         question = f'\n\nQuestion: {self.question}'
         message = ''
         for string in strings:
@@ -40,14 +40,14 @@ class OpenAPIHandler:
 
     def strings_ranked_by_relatedness(
         self,
-        query: str,
         relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
         top_n: int = 100,
     ) -> tuple[list[str], list[float]]:
         client = OpenAI(api_key=api_key)
 
-        query_embedding_response = client.embeddings.create(model='text-embedding-ada-002', input=query)
+        query_embedding_response = client.embeddings.create(model='text-embedding-ada-002', input=self.question)
         query_embedding = query_embedding_response.data[0].embedding
+
         strings_and_relatednesses = [
             (row['text'], relatedness_fn(query_embedding, row['embedding']))
             for i, row in df.iterrows()
