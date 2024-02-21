679475a7d77fe33f608d96a525b3372c3b4543ed
Evgeny Skosarev <evgeny.skosarev@adjust.com>
2024-02-21 18:43:37 +0100

    get data for fragments

--- constants.py
+++ constants.py
@@ -1,4 +1,10 @@
 GPT_MODEL = 'gpt-4-1106-preview'
 EMBEDDING_MODEL = 'text-embedding-3-small'
 MAX_TOKENS = 8191
-BATCH_SIZE = 2048
+# openai.RateLimitError: Error code: 429 - {‘error’: {‘message’: ‘Request too large for text-embedding-3-small
+# in organization org-xxx on tokens per min (TPM): Limit 1000000, Requested 1026047.
+# The input or output tokens must be reduced in order to run successfully.
+# Visit https://platform.openai.com/account/rate-limits to learn more.’}}
+BATCH_SIZE = 1000  # 2048 is a maximum value, but to handle RateLimitError this value must be reduced
+
+CONTENTFUL_LIMIT = 1000
--- download_documentation.py
+++ download_documentation.py
@@ -5,23 +5,57 @@ import pandas as pd
 from dotenv import load_dotenv
 from openai import OpenAI
 
-from constants import BATCH_SIZE, EMBEDDING_MODEL, MAX_TOKENS
+from constants import BATCH_SIZE, EMBEDDING_MODEL, MAX_TOKENS, CONTENTFUL_LIMIT
 from helpers import num_tokens
 
 load_dotenv()
 api_key = os.environ.get('OPENAI_API_KEY')
 client = OpenAI(api_key=api_key)
 
+contentful_client = contentful.Client(
+    os.environ.get('CONTENTFUL_SPACE_ID'),
+    os.environ.get('CONTENTFUL_TOKEN'),
+    timeout_s=10,
+)
+
+
+def get_entries(content_type):
+    all_entries = []
+    skip = 0
+
+    entries = contentful_client.entries(
+        {
+            'content_type': content_type,
+            'skip': skip,
+            'limit': CONTENTFUL_LIMIT,
+        }
+    )
+    all_entries.extend(entries.items)
+
+    if entries.total > entries.limit:
+        while entries.total > entries.limit:
+            skip += CONTENTFUL_LIMIT
+            entries = contentful_client.entries(
+                {
+                    'content_type': content_type,
+                    'skip': skip,
+                    'limit': CONTENTFUL_LIMIT
+                }
+            )
+            all_entries.extend(entries.items)
+
+    return all_entries
+
 
 def get_pages():
-    contentful_client = contentful.Client(
-        os.environ.get('CONTENTFUL_SPACE_ID'),
-        os.environ.get('CONTENTFUL_TOKEN'),
-        timeout_s=10,
-    )
-    entries = contentful_client.entries({'content_type': 'article', 'limit': 1000})
+    article_entries = get_entries('article')
+    fragment_entries = get_entries('fragment')
+    combined_entries = [entry for entry in article_entries + fragment_entries if 'body' in entry.raw['fields']]
 
-    return [entry.raw['fields']['title'] + '\n\n' + entry.raw['fields']['body'] for entry in entries.items]
+    return [
+        entry.raw['fields']['title'] + '\n\n' + entry.raw['fields']['body']
+        for entry in combined_entries
+    ]
 
 
 def split_page(page):
